56563f59aad582e3c87a5fce09e753376420ca8b
sravani1133 <shravanimandhadi1996>
2019-11-16 14:55:36 +0530

    commented

--- src/com/selenium/automation/Amazon.java
+++ src/com/selenium/automation/Amazon.java
@@ -6,24 +6,22 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-public class Amazon
-{
+public class Amazon {
 
-	public static void main(String[] args)
-	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sravani\\eclipse-workspace\\AutomationPrograms\\Drivers\\chromedriver.exe");
-		RemoteWebDriver driver =new ChromeDriver();
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Sravani\\eclipse-workspace\\AutomationPrograms\\Drivers\\chromedriver.exe");
+		RemoteWebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.Amazon.com");
-		
+
 		System.out.println("Updated by Rk");
-		driver.manage().timeouts().implicitlyWait(4000,	TimeUnit.SECONDS );
-	WebElement	us=driver.findElementByXPath("//input[@type='email']");
-	us.clear();
-	us.click();
-	us.sendKeys("9701882276");
 
-                                                                                                                                                                                              
-                                         }
+		driver.manage().timeouts().implicitlyWait(4000, TimeUnit.SECONDS);
+		WebElement us = driver.findElementByXPath("//input[@type='email']");
+		us.clear();
+		us.click();
+		us.sendKeys("9701882276");
 
+	}
 }
